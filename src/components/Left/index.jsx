fef4bac2ebbfbd2a1813ebbc4b3982a6be887e30
houqiang <houqiang0219@gmail.com>
2022-08-17 21:20:44 +0800

    feat 导航栏2

---
+++ src/components/Left/index.jsx
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import { MenuFoldOutlined,MenuUnfoldOutlined,TableOutlined,SmileOutlined,CrownOutlined,} from "@ant-design/icons";
+
+import {Layout,Menu} from "antd";
+import { useNavigate,} from "react-router-dom";
+
+const {Sider } = Layout;
+
+export default function LeftMenu() {
+    const [collapsed, setCollapsed] = useState(false);
+    const navigate = useNavigate();
+  return (
+    <Sider
+    trigger={null}
+    collapsible
+    collapsed={collapsed}
+    style={{ height: "100vh" }}
+  >
+    <div style={{ display: "flex", width: "200px" }}>
+      <div className="logo" />
+      <h1 className="logo-item">Ant Design Pro</h1>
+    
+    </div>
+    {React.createElement(
+          collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+          {
+            className: "trigger",
+            onClick: () => {
+                setCollapsed(!collapsed)},
+          }
+        )}
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={["1"]}
+      onClick={(e) => {
+        navigate(e.key);
+      }}
+      items={[
+        {
+          key: "welcome",
+          icon: <SmileOutlined />,
+          label: "欢迎",
+        },
+        {
+          key: "User",
+          icon: <CrownOutlined />,
+          label: "管理页",
+          children: [{ key: "admin", label: "二级管理页" }],
+        },
+        {
+          key: "list",
+          icon: <TableOutlined />,
+          label: "查询表格",
+        },
+      ]}
+    />
+   
+  </Sider>
+  )
+}
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,93 +1,53 @@
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  TableOutlined,
-  SmileOutlined,
-  CrownOutlined,
   SearchOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, Button, Input, Popconfirm } from "antd";
+import { Layout, Button, Input, Popconfirm } from "antd";
 import React, { useState } from "react";
+import LeftMenu from "../../components/Left";
 import "./index.css";
-import { useNavigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-const { Header, Sider, Content } = Layout;
+const { Header, Content } = Layout;
 const App = () => {
-  const navigate = useNavigate();
-  const [collapsed, setCollapsed] = useState(false);
   const [btn, setBtn] = useState(true);
   return (
     <Layout>
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        style={{ height: "100vh" }}
-      >
-        <div style={{ display: "flex", width: "200px" }}>
-          <div className="logo" />
-          <h1 className="logo-item">Ant Design Pro</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          onClick={(e) => {
-            navigate(e.key);
-          }}
-          items={[
-            {
-              key: "welcome",
-              icon: <SmileOutlined />,
-              label: "欢迎",
-            },
-            {
-              key: "User",
-              icon: <CrownOutlined />,
-              label: "管理页",
-              children: [{ key: "admin", label: "二级管理页" }],
-            },
-            {
-              key: "list",
-              icon: <TableOutlined />,
-              label: "查询表格",
-            },
-          ]}
-        />
-      </Sider>
+      <LeftMenu/>
+    
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }}>
           <div className="header-wrap">
-           
-              <div style={{ width: "200px", height: "64px",lineHeight:'64px',}}
-               >
+            <div style={{ width: "200px", height: "64px", lineHeight: "64px" }}>
               {btn ? (
                 <Button
                   type="primary"
                   shape="circle"
-                  style={{marginLeft:'150px'}}
+                  style={{ marginLeft: "150px" }}
                   onClick={() => {
                     setBtn(!btn);
                   }}
                   icon={<SearchOutlined />}
                 />
               ) : (
-               <div>
-                 <Button
-                type="primary"
-                shape="circle"
-                style={{marginRight:'5px'}}
-                onClick={() => {
-                  setBtn(!btn);
-                }}
-                icon={<SearchOutlined />}
-              />
-                <Input placeholder="站内搜索" style={{marginTop:'15px',width:'150px'}} /> 
-              </div> 
+                <div>
+                  <Button
+                    type="primary"
+                    shape="circle"
+                    style={{ marginRight: "5px" }}
+                    onClick={() => {
+                      setBtn(!btn);
+                    }}
+                    icon={<SearchOutlined />}
+                  />
+                  <Input
+                    placeholder="站内搜索"
+                    style={{ marginTop: "15px", width: "150px" }}
+                  />
+                </div>
               )}
-               </div>
-        
+            </div>
+
             <span className="user-name">user.name</span>
             <span className="user-logout">
               <Popconfirm
@@ -101,13 +61,7 @@ const App = () => {
           </div>
         </Header>
         <hr />
-        {React.createElement(
-          collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-          {
-            className: "trigger",
-            onClick: () => setCollapsed(!collapsed),
-          }
-        )}
+
         <Content
           className="site-layout-background"
           style={{
